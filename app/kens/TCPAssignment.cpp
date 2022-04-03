1be2a1bd91f14436012e4d2accf6ef5c4500a62a
Adlet <eypk777@gmail.com>
2022-04-03 15:21:48 +0900

    part 1, 2 syscall header에 포함 및 정의

--- app/kens/TCPAssignment.cpp
+++ app/kens/TCPAssignment.cpp
@@ -22,7 +22,9 @@ TCPAssignment::TCPAssignment(Host &host)
 
 TCPAssignment::~TCPAssignment() {}
 
-void TCPAssignment::initialize() {}
+void TCPAssignment::initialize() {
+
+}
 
 void TCPAssignment::finalize() {}
 
@@ -30,16 +32,17 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid,
                                    const SystemCallParameter &param) {
 
   // Remove below
-  (void)syscallUUID;
-  (void)pid;
+  //(void)syscallUUID;
+  //(void)pid;
 
   switch (param.syscallNumber) {
   case SOCKET:
-    // this->syscall_socket(syscallUUID, pid, std::get<int>(param.params[0]),
-    //                      std::get<int>(param.params[1]));
+    //2번째 인자 추가함
+     this->syscall_socket(syscallUUID, pid, std::get<int>(param.params[0]),
+            std::get<int>(param.params[1]), std::get<int>(param.params[2]));
     break;
   case CLOSE:
-    // this->syscall_close(syscallUUID, pid, std::get<int>(param.params[0]));
+     this->syscall_close(syscallUUID, pid, std::get<int>(param.params[0]));
     break;
   case READ:
     // this->syscall_read(syscallUUID, pid, std::get<int>(param.params[0]),
@@ -52,43 +55,76 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid,
     //                     std::get<int>(param.params[2]));
     break;
   case CONNECT:
-    // this->syscall_connect(
-    //     syscallUUID, pid, std::get<int>(param.params[0]),
-    //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
-    //     (socklen_t)std::get<int>(param.params[2]));
+     this->syscall_connect(
+         syscallUUID, pid, std::get<int>(param.params[0]),
+         static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
+         (socklen_t)std::get<int>(param.params[2]));
     break;
   case LISTEN:
-    // this->syscall_listen(syscallUUID, pid, std::get<int>(param.params[0]),
-    //                      std::get<int>(param.params[1]));
+     this->syscall_listen(syscallUUID, pid, std::get<int>(param.params[0]),
+                          std::get<int>(param.params[1]));
     break;
   case ACCEPT:
-    // this->syscall_accept(
-    //     syscallUUID, pid, std::get<int>(param.params[0]),
-    //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
-    //     static_cast<socklen_t *>(std::get<void *>(param.params[2])));
+     this->syscall_accept(
+         syscallUUID, pid, std::get<int>(param.params[0]),
+         static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
+         static_cast<socklen_t *>(std::get<void *>(param.params[2])));
     break;
   case BIND:
-    // this->syscall_bind(
-    //     syscallUUID, pid, std::get<int>(param.params[0]),
-    //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
-    //     (socklen_t)std::get<int>(param.params[2]));
+     this->syscall_bind(
+         syscallUUID, pid, std::get<int>(param.params[0]),
+         static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
+         (socklen_t)std::get<int>(param.params[2]));
     break;
   case GETSOCKNAME:
-    // this->syscall_getsockname(
-    //     syscallUUID, pid, std::get<int>(param.params[0]),
-    //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
-    //     static_cast<socklen_t *>(std::get<void *>(param.params[2])));
+     this->syscall_getsockname(
+         syscallUUID, pid, std::get<int>(param.params[0]),
+         static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
+         static_cast<socklen_t *>(std::get<void *>(param.params[2])));
     break;
   case GETPEERNAME:
-    // this->syscall_getpeername(
-    //     syscallUUID, pid, std::get<int>(param.params[0]),
-    //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
-    //     static_cast<socklen_t *>(std::get<void *>(param.params[2])));
+     this->syscall_getpeername(
+         syscallUUID, pid, std::get<int>(param.params[0]),
+         static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
+         static_cast<socklen_t *>(std::get<void *>(param.params[2])));
     break;
   default:
     assert(0);
   }
 }
+
+int TCPAssignment::syscall_socket(UUID syscallUUID, int pid, int domain, int type, int protocol){
+  return 1;
+}
+
+int TCPAssignment::syscall_close(UUID syscallUUID, int pid, int domain){
+  return 1;
+}
+
+int TCPAssignment::syscall_connect(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t addrlen){
+  return 1;
+}
+
+int TCPAssignment::syscall_listen(UUID syscallUUID, int pid, int sockfd, int backlog){
+  return 1;
+}
+
+int TCPAssignment::syscall_accept(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t * addrlen){
+  return 1;
+}
+
+int TCPAssignment::syscall_bind(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t addrlen){
+  return 1;
+}
+
+int TCPAssignment::syscall_getsockname(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t * addrlen){
+  return 1;
+}
+
+int TCPAssignment::syscall_getpeername(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t * addrlen){
+  return 1;
+}
+
 
 void TCPAssignment::packetArrived(std::string fromModule, Packet &&packet) {
   // Remove below
--- app/kens/TCPAssignment.hpp
+++ app/kens/TCPAssignment.hpp
@@ -35,6 +35,16 @@ protected:
   virtual void systemCallback(UUID syscallUUID, int pid,
                               const SystemCallParameter &param) final;
   virtual void packetArrived(std::string fromModule, Packet &&packet) final;
+
+private:
+  virtual int syscall_socket(UUID syscallUUID, int pid, int domain, int type, int protocol);
+  virtual int syscall_close(UUID syscallUUID, int pid, int domain);
+  virtual int syscall_connect(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t addrlen);
+  virtual int syscall_listen(UUID syscallUUID, int pid, int sockfd, int backlog);
+  virtual int syscall_accept(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t * addrlen);
+  virtual int syscall_bind(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t addrlen);
+  virtual int syscall_getsockname(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t * addrlen);
+  virtual int syscall_getpeername(UUID syscallUUID, int pid, int sockfd, struct sockaddr * addr, socklen_t * addrlen);
 };
 
 class TCPAssignmentProvider {
